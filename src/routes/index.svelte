d903b9452a36545f031669e1b932c9908ca78141
aaronconway7 <aaronconway796@gmail.com>
2021-06-29 20:29:42 +0100

    backing up files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,15 @@
+<script>
+    // const netlifyIdentity = require('netlify-identity-widget')
+    import { onMount } from 'svelte'
+    import netlifyIdentity from 'netlify-identity-widget'
+
+    onMount(() => {
+        netlifyIdentity.init()
+        netlifyIdentity.open()
+        netlifyIdentity.on('login', user => console.log('login', user))
+    })
+</script>
+
 <svelte:head>
     <title>Fifa Series</title>
 </svelte:head>
